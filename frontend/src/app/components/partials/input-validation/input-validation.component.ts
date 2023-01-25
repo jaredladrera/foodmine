d73e2843fb4e49ea7f68c35e4374900963b113e0
lance jared cabiscuelas <ladrera21@gmail.com>
2023-01-25 11:08:19 +0800

    ercover to company laptop

--- frontend/src/app/components/partials/input-validation/input-validation.component.ts
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 const VALIDATOR_MESSAGES: any = {
@@ -11,15 +11,39 @@ const VALIDATOR_MESSAGES: any = {
   templateUrl: './input-validation.component.html',
   styleUrls: ['./input-validation.component.css']
 })
-export class InputValidationComponent implements OnInit {
+export class InputValidationComponent implements OnInit, OnChanges {
 
   @Input() control!: AbstractControl;
   @Input() showErrorWhen: boolean = true;
   @Input() errorMessages: string[] = []
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation()
+  }
 
   ngOnInit(): void {
+
+    //for validation status change
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation();
+    });
+
+    //for input value change
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    })
+  }
+
+  checkValidation() {
+    const errors = this.control.errors;
+    if(!errors) {
+      this.errorMessages = [];
+      return;
+    }
+
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map(key => VALIDATOR_MESSAGES[key]);
   }
 
 }
