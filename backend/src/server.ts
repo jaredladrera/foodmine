fd02c2cd6522624b804e8b0f5feadd8f1158d40f
lance jared cabiscuelas <ladrera21@gmail.com>
2023-01-29 13:58:14 +0800

    working on the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors'
-import { sample_foods, sample_tags, sample_users } from './data';
-import jwt from 'jsonwebtoken';
+import foodRouter from './router/food.router';
+import userRouter from './router/user.router';
 
 const app = express();
 app.use(express.json());
@@ -10,58 +10,9 @@ app.use(cors({
     origin: ["http://localhost:4200"]
 }));
 
-app.get("/api/foods", (req, res) => {
-    res.send(sample_foods);
-});
+app.use("/api/foods", foodRouter);
+app.use('/api/user', userRouter);
 
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-    const searchTerm = req.params.searchTerm;
-    const foods =  sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
-
-    res.send(foods);
-});
-
-app.get('/api/foods/tags', (req, res) => {
-    res.send(sample_tags);
-})
-
-app.get('/api/foods/tag/:tagName', (req, res) => {
-    const tagName = req.params.tagName;
-    // console.log(tagName);
-    const food = sample_foods.filter(t => t.tags?.includes(tagName));
-    res.send(food);
-});
-
-app.get('/api/foods/:foodId', (req, res) => {
-    const foodId = req.params.foodId;
-    const food = sample_foods.find(food => food.id == foodId);
-
-    res.send(food);
-});
-
-app.post('/api/user/login', (req, res) => {
-    // const body = req.body;
-    const { email, password } = req.body;
-    console.log(req.body);
-    const user = sample_users.find(user => user.email === email && user.password === password)
-
-    if(user) {
-        res.send(generateTokenResponse(user));
-    } else {
-        res.status(400).send("Username or password not found");
-    }
-
-});
-
-const generateTokenResponse = function (user: any) {
-    const token = jwt.sign({
-        email: user.email,
-        isAdmin: user.isAdmin
-    }, "SomeRandomText", { expiresIn: "30d" });
-
-    user.token = token;
-    return user;
-}
 
 const port = 5000;
 
